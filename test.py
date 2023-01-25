21196e3b4badf384d243f0ef361839ac6086ffad
sheetal0797 <54700517+sheetal0797@users.noreply.github.com>
2023-01-25 17:04:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 #test case for adding two numbers
 import unittest
 from prog1 import summation
@@ -7,6 +7,7 @@ class TestSum(unittest.TestCase):
 	  	"""
 	  	test case to add two numbers
 	  	"""
+		print("test")
 		data=[23,32]
 		result=summation(data)
 		self.assetEqual(retult,55)
