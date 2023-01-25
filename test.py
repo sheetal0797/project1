09d406d323dd4c656419057f3d8b6a2b569b19db
sheetal0797 <54700517+sheetal0797@users.noreply.github.com>
2023-01-25 17:09:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ class TestSum(unittest.TestCase):
 		print("test")
 		data=[23,32]
 		result=summation(data)
-		self.assetEqual(retult,55)
+		self.assetEqual(result,55)
 if __name__=='__main__':
 	unittest.main()
